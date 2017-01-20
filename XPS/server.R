d20aaf8bd8a2519c0b952a9021ac3ba7a46c10d5
NikolasEnt <nikolasent@gmail.com>
2017-01-20 15:49:47 +0300

    Comments and code formatting improved

--- XPS/server.R
+++ XPS/server.R
@@ -12,42 +12,35 @@ shinyServer(function(input, output) {
   sum <- 0
   max_rows<-0
   decs <- ","
-  
   in_data<-reactive({
     inFile <- input$file_to_open
-    
     if (is.null(inFile))
       return(NULL)
-    
+    #Read input file
     d = read.table(inFile$datapath, sep=" ", fill=FALSE, strip.white=TRUE)
     df<-data.frame(d[,1], d[,3])
-    # number_of_rows <- 1
-    # number_of_columns <- 1
     while((df[number_of_rows+1,1]-df[number_of_rows,1])>0){
       number_of_rows<<-number_of_rows+1
     }
     number_of_columns<<-dim(df)[1]/number_of_rows
-    
+    #Render interactive UI elements
     output$range_slider <- renderUI({
       sliderInput("Range_Slider",label="Choose the ROI",min = 1,max = number_of_columns,value = c(5,number_of_columns-5), step=1)
     })
     output$angle_slider <- renderUI({
-      sliderInput("angle", label = "Rotate ange of the 3D plot", min=0, max=90, value = 75, step=1)
+      sliderInput("angle", label = "Rotate angle of the 3D plot", min=0, max=90, value = 75, step=1)
     })
     output$help_text <- renderText( "Upload a batch of files in order to process them by subsampling the ROI and computing cumulative spectra. Use \"Process!\" button to download a .csv file with the spectra.")
     
     output$batchfiles <- renderUI({
       fileInput('files_to_open', 'Choose .xy files to process with the selected above settings', accept=c('.xy'), multiple = TRUE)
     })
-    
     output$preview_button <- renderUI({
       actionButton("preview", "Preview")
     })
-    
     output$process_button <- renderUI({
       downloadButton("process", "Process!")
     })
-    
     if(input$entype=="be"){
       xlabel <<- "Binding Energy (B.E.), eV"
       data_spec<<-data.frame(input$exe-df[1:number_of_rows,1])
@@ -57,7 +50,6 @@ shinyServer(function(input, output) {
       data_spec<<-data.frame(df[1:number_of_rows,1])
     }
     
-    
     colnames(data_spec)[1]<-xlabel
     pointer=1
     for (i in c(2:(number_of_columns+1))){
@@ -65,7 +57,6 @@ shinyServer(function(input, output) {
       pointer<-pointer+number_of_rows
       colnames(data_spec)[i]<<-paste("Curve ", toString(i-2))
     }
-    
   })
 
   
@@ -73,29 +64,23 @@ observeEvent(input$entype,{
   in_data()
 })
 
-
 observeEvent(input$go,{
   output$distPlot <- renderPlot({
     in_data()
-    
     colMax <- function(data) sapply(data, max, na.rm = TRUE)
     rowMax <- function(data) do.call(pmax, data)
-    
     data_spec_sm<-data_spec
     data_spec_sm[,(2:(input$Range_Slider[1]))] <- 0
     data_spec_sm[,((input$Range_Slider[2]+2):(number_of_columns+1))] <- 0
-    
     ang <- input$angle
     cpr <- number_of_columns
     if(input$entype=="be"){
       #ang <- ang+90
       #cpr <- 1
     }
-    
     x=data_spec[,1]
     xlimit=range(data_spec[,1])
-    
-    
+    #Draw 3D plot
     f <- function(i, j) { r <- data_spec_sm[i,j] }
     z <- f(1:number_of_rows,2:(number_of_columns+1))
     y <- seq(1,number_of_columns,length=number_of_rows)
@@ -107,7 +92,6 @@ observeEvent(input$go,{
       x=rev(x)
       ma.x <- rev(matrix(x,nrow=length(x),ncol=1))
     }
-    
     scatterplot3d(x, y, za, type = "n", box = FALSE,
                  angle = ang, xlim=xlimit, x.ticklabs = NA, xlab = xlabel, ylab = "Channel #",
                  zlab = "Cps", main = paste("Plot of the input file ", toString(input$file_to_open[1])))->sp
@@ -120,19 +104,18 @@ observeEvent(input$go,{
     sp$points3d(x, y=rep.int(input$Range_Slider[2],number_of_rows),z=data_spec_sm[,(input$Range_Slider[2]+1)],
                 type = "l", lwd=1, col="red", xlim=xlimit)
     max_z <- colMax(data_spec_sm[,2:(number_of_columns+1)])
-    sp$points3d(x=rep.int(range(data_spec[,1])[1], number_of_columns), y=seq(1,number_of_columns,length=number_of_columns),z=max_z,
+    sp$points3d(x=rep.int(range(data_spec[,1])[1], number_of_columns),
+                y=seq(1,number_of_columns,length=number_of_columns),z=max_z,
                 type = "l", lwd=1, col="blue", xlim=xlimit)
     max_zp <- rowMax(data_spec_sm[,2:(number_of_columns+1)])
     sp$points3d(x, y=rep.int(cpr,number_of_rows),z=max_zp,
                 type = "l", lwd=1, col="blue", xlim=xlimit)
   })
-  
+  #Render cumulative plot
   output$sumPlot <- renderPlot({
     sum<<-rowSums(data_spec[c(colnames(data_spec)[(input$Range_Slider[1]+1):(input$Range_Slider[2]+1)])])/1000
     g1<-ggplot(data_spec, aes(x=data_spec[,1], y=sum))+geom_line()+xlab(xlabel)+ylab("Cps, 1000*")+ggtitle("Cumulative spectrum of the ROI")
-    
     small_data_spec<-melt(data_spec[,c(1,(input$Range_Slider[1]+1):(input$Range_Slider[2]+1))], id.vars = 1)
-
     if(input$entype=="be"){
       g1 <- g1+scale_x_reverse()
     }
@@ -140,7 +123,7 @@ observeEvent(input$go,{
   })
   
 })
-
+#Set decimal separator
 observeEvent(input$septype, {
   if (input$septype=="c"){
     decs<<-","
@@ -149,45 +132,35 @@ observeEvent(input$septype, {
     decs<<-"."
   }
 })
-  
+#prepare downloadable files  
 output$download_all <- downloadHandler(
-  
   filename = function() { paste(gsub( ".xy", "", toString(input$file_to_open[1] )), '_all.csv', sep='') },
   content = function(file) {
-    
     colnames(data_spec)[1]<-xlabel
     write.table(data_spec, file,row.names=FALSE, sep=";", dec=decs)
   }
 )
-
 output$download_sum <- downloadHandler(
-
   filename = function() { paste(gsub( ".xy", "", toString(input$file_to_open[1] )), '_sum.csv', sep='') },
   content = function(file) {
-
     sumdf <- data.frame(data_spec[,1], sum)
     colnames(sumdf)[1]<-xlabel
     colnames(sumdf)[2]<-"cps, 1000*"
     write.table(sumdf, file,row.names=FALSE, sep=";", dec=decs)
   }
 )
-
 output$download_roi <- downloadHandler(
-  
   filename = function() { paste(gsub( ".xy", "", toString(input$file_to_open[1] )), '_roi.csv', sep='') },
   content = function(file) {
-
     roidf <- data_spec[,c(1,(input$Range_Slider[1]+1):(input$Range_Slider[2]+1))]
     colnames(roidf)[1]<-xlabel
     write.table(roidf, file,row.names=FALSE, sep=";", dec=decs)
   }
 )
-
-
-observeEvent(input$preview, { 
+observeEvent(input$preview, {
+  #Render preview for multiple .xy file input
   rows <- (length(input$files_to_open[,1])%/%2+length(input$files_to_open[,1])%%2)
   output$preview_plot<-renderPlot({
-    
     par(mfrow=c(rows,3))
     for (i in 1:length(input$files_to_open[,1])){
       inFile <- input$files_to_open[[i, 'name']]
@@ -223,25 +196,22 @@ observeEvent(input$preview, {
       }
       plot(x, y, xlim=xlimit, main=inFile[1], xlab=xlabel, ylab = "Cps, 1000*", type="l")
     }
-  
-    
-   
   }, width = 720, height = rows*240 ) #
 })
-
+#Downloader for the batch processed files
 output$process <- downloadHandler(
-  
   filename = function() {
-    
     paste(gsub( ".xy", "", toString(input$files_to_open[[1, 'name']])),
-          '_seria_', toString(input$Range_Slider[1]), '_', toString(input$Range_Slider[2]), '.csv', sep='')
+          '_seria_', toString(input$Range_Slider[1]), '_',
+          toString(input$Range_Slider[2]), '.csv', sep='')
     },
   content = function(file) {
     ldf<-data.frame(a=NA, b=NA)[numeric(0), ]
     max_rows<-0
     for (i in 1:length(input$files_to_open[,1])){ 
       inFile <- input$files_to_open[[i, 'name']]
-      d = read.table(input$files_to_open[[i, 'datapath']], sep=" ", fill=FALSE, strip.white=TRUE)
+      d = read.table(input$files_to_open[[i, 'datapath']],
+                     sep=" ", fill=FALSE, strip.white=TRUE)
       df<-data.frame(d[,1], d[,3])
       number_of_rows2 <- 1
       number_of_columns2 <- 1
@@ -287,14 +257,10 @@ output$process <- downloadHandler(
       else{
         ldf[1:max_rows, 1:2]<- ldf_sm
       }
-      
     }
-    
     write.table(ldf[,3:length(ldf)], file,row.names=FALSE, sep=";", dec=decs, na="")
   }
 )
-
-  
 })
 
 
--- XPS/ui.R
+++ XPS/ui.R
@@ -1,19 +1,16 @@
-
 library(shiny)
-
-# Define UI for application that draws a histogram
+# Define UI for the application
 shinyUI(fluidPage(
-  
   # Application title
   titlePanel("XPS visualizer and converter"),
   h5(tags$i("ver. 1.1.0")),
-  # Sidebar with a slider input for number of bins 
+  # Sidebar with inputs
   sidebarLayout(
     sidebarPanel(
-      fileInput('file_to_open', 'Choose .xy File', accept=c('.xy')),
+      fileInput('file_to_open', 'Choose .xy File', accept=c('.xy')), #file input
       radioButtons("entype", "Set desired energy type:",
                    c("Kinetic Energy" = "ke",
-                     "Binding Energy" = "be"),
+                     "Binding Energy" = "be"), #Energy type select radiobutton
                    inline = TRUE),
       numericInput("exe", "Excitation energy, eV:", 770, min = 0, max = 10000, step=0.1),
       actionButton("go", "Upload!"),
@@ -29,10 +26,10 @@ shinyUI(fluidPage(
       h5(tags$i("e-mail: falaleevn@yandex.ru")),
       helpText(a("See source code",href="https://github.com/NikolasEnt/XPS-visualizer-and-converter"))
     ),
-    
+    #Main window with plots
     mainPanel(
-       plotOutput("distPlot"),
-       plotOutput("sumPlot"),
+       plotOutput("distPlot"), #3D plot
+       plotOutput("sumPlot"), #Cumulative plot
        radioButtons("septype", "Set desired decimal separator:",
                     c("comma (,)" = "c",
                       "period (.)" = "p"),
